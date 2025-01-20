e8316d7f7b327bd2611df9e362876c4cb1480d96
fivaleriano <filipevaleriano85@gmail.com>
2025-01-20 04:43:14 +0000

    ATL_SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,26 @@ int main() {
      printf("Desafio cartas super trunfo!\n");
 
 
+/*Propriedades da primeira*/
 
-    return 0;
+     char pais[60];
+     char estado[30];
+     char A01;
+     char cidade[50];
+     int populacao;
+     float are;
+     float pib;
+     int npt;
+
+printf("Selecione a carta:\n");
+scanf(" %s", &A01);
+printf("Estado: %s.\n", estado);
+printf("Cidade: %s.\n", cidade);
+printf("População: %d.\n", populacao);
+printf("Área: %f.\n", are);
+printf("PIB: %f.\n", pib);
+printf("Numero de pontos turísticos: %d\n.", npt);
+
+
+      
 }
