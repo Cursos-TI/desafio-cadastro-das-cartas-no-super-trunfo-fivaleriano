e5369d2e036443557e9196576b0c38f3107f87c6
fivaleriano <filipevaleriano85@gmail.com>
2025-01-21 00:06:50 +0000

    NTD_COMMIT

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,26 +7,33 @@ int main() {
      printf("Desafio cartas super trunfo!\n");
 
 
-/*Propriedades da primeira*/
+/*Propriedades da primeira carta*/
 
-     char pais[60];
-     char estado[30];
-     char A01;
+     char codigo[30];
+     char letra[10];
      char cidade[50];
-     int populacao;
+     float popula;
      float are;
      float pib;
      int npt;
 
-printf("Selecione a carta:\n");
-scanf(" %s", &A01);
-printf("Estado: %s.\n", estado);
-printf("Cidade: %s.\n", cidade);
-printf("População: %d.\n", populacao);
-printf("Área: %f.\n", are);
-printf("PIB: %f.\n", pib);
-printf("Numero de pontos turísticos: %d\n.", npt);
+printf("Digite a letra do estado:\n");
+scanf(" %s", letra);
+printf("Digite o código da carta:\n");
+scanf(" %s", codigo);
+printf("Digite o nome cidade:\n");
+scanf(" %s", cidade);
+printf("Digite a população da cidade:\n");
+scanf(" %f", popula);
+printf("Digite o tamanho da área:\n");
+scanf(" %f", are);
+printf("Digite o PIB da cidade:\n");
+scanf(" %f", pib);
+printf("Digite a quantidade de pontos turísticos:\n");
+scanf("%d", npt);
 
 
+
+   
       
 }
