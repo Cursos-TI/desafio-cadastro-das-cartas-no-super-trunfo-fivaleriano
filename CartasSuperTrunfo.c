0afb518cc1a8ca33a0ea0d1140e914c140bf4a98
fivaleriano <filipevaleriano85@gmail.com>
2025-02-06 01:22:36 +0000

    DST_NNfi1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,10 @@
 
 int main() {
 
-     printf("Desafio cartas super trunfo!\n");
+     printf("######   Desafio nivel novato cartas super trunfo!  ######\n");
 
 
-/*Propriedades da primeira carta*/
+  // VARIAVEIS DAS CARTAS
 
      char codigo[30];
      char letra[10];
@@ -17,30 +17,44 @@ int main() {
      float pib;
      int npt;
 
+  // ENTRADA DE DADOS
+
 printf("Digite a letra do estado:\n");
        scanf(" %s", &letra);
+
 printf("Digite o código da carta:\n");
        scanf(" %s", &codigo);
+
 printf("Digite o nome cidade:\n");
        scanf(" %s", &cidade);
+
 printf("Digite a população da cidade:\n");
        scanf("%f", &popula);
+
 printf("Digite o tamanho da área:\n");
        scanf("%f", &are);
+
 printf("Digite o PIB da cidade:\n");
        scanf("%f", &pib);
+
 printf("Digite a quantidade de pontos turísticos:\n");
        scanf("%d", &npt);
+
+
+  // SAIDA DE DADOS
 
 
 printf("Estado: %s\n", letra);
 printf("Codigo da carta: %s\n", codigo);
 printf("Nome da cidade: %s\n", cidade);
-printf("População: %f\n", popula);
-printf("Área: %f km²\n", are);
-printf("PIB: %f\n", pib);
+printf("População: %.2f milhões de habitantes\n", popula);
+printf("Área: %.2f km²\n", are);
+printf("PIB: R$%.2f milhões de reais\n", pib);
 printf("Numero de pontos turísticos: %d\n", npt);
 
    
+
+   return 0;
+
       
 }
