c33b4a9d04767fb30da6d7c535088187be72907e
fivaleriano <filipevaleriano85@gmail.com>
2025-02-24 23:37:01 +0000

    COMMIT_EDT_ST

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,13 +4,14 @@ int main () {
 
       // Variaveis para os dados das cartas do jogador 1 e jogador 2.
 
-char estado, estado02;
+char* estado, estado02;
 char* codigoCarta[5], codigoCarta02[5];
-char nomecidade[50], nomecidade02[50];
+char* nomecidade[50], nomecidade02[50];
 float populacao, populacao02;
 float area, area02;
 float pib, pib02;
 int npt, npt02;
+float sp, sp2;
 
 
                    // Início do jogo de coleta de dados.
@@ -87,6 +88,12 @@ printf("###### Preencha os dados das cartas do jogador 02 ######\n");
      
                 ppc2 = pib02 / populacao02;
 
+                // Calculo para definição do Super Poder!
+
+           sp = (float)populacao + area + pib + npt + ppc + dp; // calculo para o super poder da primeira carta.
+
+           sp2 = (float)populacao02 + area02 + pib02 + npt02 + ppc2 + dp2; // calculo para o super poder da segunda carta.
+
       
 
                    //Exibição dos dados das cartas ja preenchidas do jogador 1.
@@ -96,6 +103,7 @@ printf("###### Dados da carta do jogador 01 ######\n");
      printf("Estado: %c\nCodigo da carta: %s\nNome da cidade: %s\nPopulação: %.2f\n", estado, codigoCarta, nomecidade, populacao);
      printf("Área: %.2f km²\nPIB: R$ %.2f bilhoes de reais\nNumero de pontos turisticos: %d\n", area, pib, npt);
      printf("Densidade populacional: %.2f pessoas/km²\nPIB per capita: %.2f reais\n", dp, ppc);
+     printf("Super Poder:%.2f\n", sp);
 
 
                   //Exibição dos dados das cartas ja preenchidas do jogador 2.
@@ -106,6 +114,21 @@ printf("###### Dados da carta do jogador 02 ######\n");
      printf("Estado: %c\nCodigo da carta: %s\nNome da cidade: %s\nPopulação: %.2f\n", estado02, codigoCarta02, nomecidade02, populacao02);
      printf("Área: %.2f km²\nPIB: R$ %.2f bilhoes de reais\nNumero de pontos turisticos: %d\n", area02, pib02, npt02);
      printf("Densidade populacional: %.2f pessoas/km²\nPIB per capita: %.2f reais\n", dp2, ppc2);
+     printf("Super Poder:%.2f\n", sp2);
+
+
+      // Comparação entre os dados da primeira e segunda carta.
+
+printf("###### Resultado da comparação entre as cartas ######\n");
+
+
+     printf("População:%d\n", populacao > populacao02); // comparação entre população da primeira e segunda carta.
+     printf("Area:%d\n", area > area02); // comparação da Area da primeira e segunda carta.
+     printf("PIB:%d\n", pib > pib02); // comparação do PIB da primeira e segunda carta.
+     printf("Numero de pontos turisticos:%d\n", npt > npt02); // comparação da quantidade de pontos turisticos da primeira e segunda carta.
+     printf("Densidade populacional:%d\n", dp < dp2); // comparação da densidade populacional. (OBS: nesse caso o vencedor e o que tiver o menor valor).
+     printf("PIB per capita:%d\n", ppc > ppc2); // comparação do PIB per capita da primeira e segunda carta.
+     printf("Super Poder:%d\n", sp > sp2); // comparação do super poder da primeira e segunda carta.
 
 
      
